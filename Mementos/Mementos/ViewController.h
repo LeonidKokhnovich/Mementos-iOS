97fcde5435ebf07932e418bd500c32df22aa8591
Leonid Kokhnovich <leonid.kokhnovich@developex.com>
2014-11-17 15:32:21 +0200

    Initial commit

---
+++ Mementos/Mementos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mementos
+//
+//  Created by Leonid Kokhnovych on 11/17/14.
+//  Copyright (c) 2014 MyOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mementos/Mementos/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Mementos
+//
+//  Created by Leonid Kokhnovych on 11/17/14.
+//  Copyright (c) 2014 MyOrganization. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
